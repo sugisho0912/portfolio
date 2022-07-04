d1c152dad46360f54098b46dc292e89e625a5d07
sugisho0912 <92170047+sugisho0912@users.noreply.github.com>
2022-07-04 13:14:14 +0900

    main_anima

---
+++ anim.js
@@ -0,0 +1,7 @@
+var animationTivel = lottie.loadAnimation({
+    container: document.getElementById('tivel'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'web_anim_data.json'
+    });
