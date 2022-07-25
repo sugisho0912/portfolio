394fb3acf06742cb81c9b303fcef3d6c0cc75c73
sugisho0912 <92170047+sugisho0912@users.noreply.github.com>
2022-07-25 09:02:20 +0900

    gazou nyuuryoku

--- anim.js
+++
@@ -1,7 +0,0 @@
-var animationTivel = lottie.loadAnimation({
-    container: document.getElementById('tivel'),
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
-    path: 'web_anim_data.json'
-    });
